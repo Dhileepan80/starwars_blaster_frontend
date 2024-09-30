465561d17ef324f05e535ee7b811e6bfe94c9e78
dhileepan08 <dhileepan@sketchbrahma.com>
2024-09-30 12:43:31 +0530

    qf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    build: {
+      // sourcemap: mode === 'development',
+    sourcemap: env.VITE_GENERATE_SOURCEMAP === 'true',
+    }
+  }
 })
